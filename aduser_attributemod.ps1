c72ae6ad5d5c9ee0539ae3a57a12aea439a93657
oliverfarthing <124591524+oliverfarthing@users.noreply.github.com>
2023-11-02 15:49:06 +0000

    Add files via upload

---
+++ aduser_attributemod.ps1
@@ -0,0 +1,3 @@
+﻿Get-ADUser -Filter * -SearchBase 'OU=Bramptonvalley Users,OU=Winvic Users,DC=Winvic,DC=local' | Foreach-Object{
+   Set-ADUser -Identity $_ -Email "$($_.Name -replace ' ', '')@bramptonvalleyhomes.co.uk"
+}
