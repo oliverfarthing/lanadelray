17ef1053d17003e6b6478155e8307397a860a6c3
oliverfarthing <124591524+oliverfarthing@users.noreply.github.com>
2023-11-14 15:46:38 +0000

    Add files via upload

---
+++ detectioncodec.ps1
@@ -0,0 +1,6 @@
+﻿if (Test-Path -Path "$env:LOCALAPPDATA\Procore Technologies\Procore Drive\Procore DriveUninstaller.exe") {
+    Write-Host "Found it!"
+}
+elseif (Test-Path -Path "C:\Program Files (x86)\Procore Technologies\Procore Drive\Procore DriveUninstaller.exe") {
+Write-Host "Found it!"
+}
